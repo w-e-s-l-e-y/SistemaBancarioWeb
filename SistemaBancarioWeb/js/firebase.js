573fddf2627881f6918ff478698a30ecdfda368a
w-e-s-l-e-y <marquezrogerio6@gmail.com>
2024-05-03 08:57:57 -0300

    chave firebase

---
+++ SistemaBancarioWeb/js/firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAtIGxA5l2gtDiA1XkGieUmFfWKQycEe3Q",
+    authDomain: "wykbank-5b21d.firebaseapp.com",
+    databaseURL: "https://wykbank-5b21d-default-rtdb.firebaseio.com",
+    projectId: "wykbank-5b21d",
+    storageBucket: "wykbank-5b21d.appspot.com",
+    messagingSenderId: "258416889029",
+    appId: "1:258416889029:web:44f6c7a611ee43116174ac",
+    measurementId: "G-SGMM5N5BXW"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
